655783a2235c892b02ff3e2d2a870c86be572d72
LucasOliveira2077 <lucasdeoliveira.95@outlook.com>
2022-03-19 11:49:36 -0300

    new lines of code

--- noticias.js
+++ noticias.js
@@ -1,2 +1,10 @@
 var http = require('http');
 
+var server = http.createServer( function (req, res){
+
+    res.end("<html><body>Portal de Noticias</body></html>");
+
+    
+});
+
+server.listen(3000);
