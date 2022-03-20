14bff704cfec7f13fc2171786d7233bc1a9b917a
LucasOliveira2077 <lucasdeoliveira.95@outlook.com>
2022-03-20 19:41:33 -0300

    Some changes

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,10 @@
-module.exports = (app) => {
-    app.get('/',(req, res) => {//url padrão
-        res.render("home/index");//resposta enviada do server ao cliente lendo o arquivo ejs dentro do diretorio
+let databaseConnection = require('../../config/databaseConnection');
 
+module.exports = (app) => {
+    let connection = databaseConnection();
+        app.get('/',(req, res) => {//url padrão
+            connection.query('select * from home', (error, result) =>{
+                res.render("home/index", {index : result});//resposta enviada do server ao cliente lendo o arquivo ejs dentro do diretorio
+        });
     });
 };
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -2,7 +2,7 @@
 let databaseConnection = require('../../config/databaseConnection');
 
 module.exports = (app) => {
-  let connection = databaseConnection();//conection recebe o require pra database cenection
+let connection = databaseConnection();//conection recebe o require pra database cenection
     app.get('/noticias',(req, res) => {
         connection.query('select * from noticias', (error, result) =>{
           res.render("noticias/noticias", {noticias : result});
