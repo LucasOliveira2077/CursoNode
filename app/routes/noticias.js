902b794a6e1d5f73b23cbef22d21529eeed178af
LucasOliveira2077 <lucasdeoliveira.95@outlook.com>
2022-03-20 12:09:38 -0300

    Connection with mysql server stablished

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,4 +1,20 @@
+const res = require('express/lib/response');
+
 module.exports = (app) => {
+    let mysql = require('mysql');
+
+    let connection = mysql.createConnection({ // criando uma variavel que recebe o metodo createConnection do mysql
+        host: 'localhost',
+        user: 'root',
+        password: '1006',
+        database: 'portal_noticias'
+    });// valores que passamos numa função json para o mysql interpretar
+
+    connetion.query('select * from noticias', (error, result) =>{//usando a variavel connection, fazemos uma query na noticias que espera um error e um resultado e retorna na consulta
+        res.send(result);
+    });
+
+
     app.get('/noticias',(req, res) => {
         res.render("noticias/noticia");
 
